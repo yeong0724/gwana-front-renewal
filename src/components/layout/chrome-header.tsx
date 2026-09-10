1bc14e49abb447a8edbeccbad0c37ffd067c6dea
Jinyeong Kim <kid4211@gmail.com>
2026-09-10 13:02:01 +0900

    재배포용 커밋

--- src/components/layout/chrome-header.tsx
+++ src/components/layout/chrome-header.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 import { isNonCommonRoute } from "@/constants/route-chrome";
 
 /**
+ * [ChromeHeader 컴포넌트]
  * 경로에 따라 헤더를 고르는 스위치. 이 컴포넌트만 클라이언트다.
  *
  * 두 헤더는 **RSC로 렌더된 트리를 prop으로 받는 것**이라 서버 컴포넌트로
